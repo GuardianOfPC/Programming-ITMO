bdd77f097ec5e459e5774dda0a252667e82806fc
Alexey Odinochenko <69080974+GuardianOfPC@users.noreply.github.com>
2021-04-02 13:12:35 +0300

    Add files via upload

--- lab2.cpp
+++ lab2.cpp
@@ -1,128 +1,118 @@
-#include "polynom.h"
-#include <algorithm>
-
+#include "Polynom.h"
 
 Polynom::Polynom(index ind): ind(std::move(ind))
-{};
+{}
 Polynom::Polynom() = default;
 Polynom::Polynom(const Polynom &other) = default;
 Polynom::~Polynom() = default;
 
-Polynom& Polynom::operator= (const Polynom& other){
-    if (this != &other){
+Polynom& Polynom::operator=(const Polynom& other) {
+    if (this != &other)
         ind = other.ind;
-    }
     return *this;
 }
-bool operator ==(const Polynom& x, const Polynom& y){
+
+bool operator==(const Polynom& x, const Polynom& y) {
     return x.ind == y.ind;
 }
 
-bool operator !=(const Polynom& x, const Polynom& y){
+bool operator!=(const Polynom& x, const Polynom& y) {
     return !(x == y);
 }
 
-Polynom& operator -(Polynom z){
-    for(auto& ind: z.ind){
-        ind.second = -ind.second;
-    }
-    return z;
+Polynom operator-(Polynom p) {
+    for (auto& coefficient: p.ind)
+        coefficient.second = -coefficient.second;
+    return p;
 }
 
-Polynom& Polynom::operator -(const Polynom& other){
-    return *this + (-other);
+void Polynom::operator+=(const Polynom& other) {
+    for (auto other_coefficient: other.ind)
+        ind[other_coefficient.first] += other_coefficient.second;
 }
 
-void Polynom::operator+=(const Polynom& other){
-    for (auto other_ind: other.ind){
-        ind[other_ind.first] += other_ind.second;
-    }
-}
-
-void Polynom::operator-=(const Polynom& other){
+void Polynom::operator-=(const Polynom& other) {
     *this += (-other);
 }
 
-Polynom& Polynom::operator +(const Polynom& other) const{
+Polynom Polynom::operator+(const Polynom& other) {
     Polynom result(ind);
     result += other;
     return result;
 }
 
-void Polynom::operator *=(double number){
-    for (auto& ind1 : ind){
-        ind1.second *= number;
-    }
+Polynom Polynom::operator-(const Polynom& other) {
+    return *this + (-other);
 }
 
-void Polynom::operator *= (const Polynom& other){
-    for (auto& ind1: ind){
-        for (auto other_ind: other.ind){
-            ind1.second *= other_ind.second;
-        }
-    }
+double Polynom::operator[](int index) {
+    return ind[index];
 }
 
-Polynom& Polynom::operator *(double number) const{
-    Polynom z(ind);
-    z *= number;
-    return z;
+void Polynom::operator*=(double num) {
+    for (auto& coefficient : ind)
+        coefficient.second *= num;
 }
 
-Polynom& Polynom::operator *(const Polynom& other) const{
-    Polynom z(ind);
-    z *= other;
-    return z;
+void Polynom::operator*=(const Polynom &other) {
+    for (auto& coefficient: ind)
+        for (auto other_coefficient: other.ind)
+            coefficient.second *= other_coefficient.second;
 }
 
-double Polynom::operator [](int index_p){
-    return ind[index_p];
+Polynom Polynom::operator*(double num) {
+    Polynom p(ind);
+    p *= num;
+    return p;
 }
 
-void Polynom::operator/=(double number){
-    if (number == 0){
-        throw std::runtime_error("Cannot");
-    }
-    for (auto& ind1: ind){
-        ind1.second /= number;
-    }
+Polynom Polynom::operator*(const Polynom &other) {
+    Polynom p(ind);
+    p *= other;
+    return p;
 }
 
-Polynom& Polynom::operator /(double number) const{
-    Polynom z(ind);
-    z /= number;
-    return z;
+void Polynom::operator/=(double number) {
+    if (number == 0)
+        throw std::runtime_error("Error");
+    for (auto& coefficient: ind)
+        coefficient.second /= number;
 }
 
-void Polynom::print(std::ostream &ostream, std::pair<int, double> ind1){
-    if(ind1.second < 0){
+Polynom Polynom::operator/(double num) {
+    Polynom p(ind);
+    p /= num;
+    return p;
+}
+
+void Polynom::print(std::ostream& ostream, std::pair<int, double> coefficient) {
+    if (coefficient.second < 0) {
         ostream << '-';
-        ind1.second = - ind1.second;
+        coefficient.second = -coefficient.second;
     } else {
         ostream << '+';
     }
-    ostream << " " << ind1.second << " * x^" << ind1.first << " ";
+    ostream << ' ' << coefficient.second << " * x^" << coefficient.first << ' ';
 }
 
-std::istream &operator >>(std::istream &istream, Polynom &z){
-    int ind_count;
-    istream >> ind_count;
-    std::pair<int, double> ind1;
-    while(ind_count--){
-        istream >> ind1.first;
-        istream >> ind1.second;
-        z.ind.emplace(ind1);
+std::istream &operator>>(std::istream &istream, Polynom &p) {
+    unsigned n_of_coefficients;
+    istream >> n_of_coefficients;
+    std::pair<unsigned, double> coefficient;
+    while (n_of_coefficients--) {
+        istream >> coefficient.first;
+        istream >> coefficient.second;
+        p.ind.emplace(coefficient);
     }
     return istream;
 }
 
-std::ostream &operator <<(std::ostream &ostream, const Polynom &z){
-    auto counter = z.ind.begin();
-    std::pair<int, double> first_ind = *counter;
-    ostream << first_ind.second << " * x^" << first_ind.first << " ";
-    for (++counter; counter != z.ind.end(); ++counter){
+std::ostream &operator<<(std::ostream &ostream, const Polynom &p) {
+    auto counter = p.ind.begin();
+    std::pair<int, double> ind_first = *counter;
+    ostream << ind_first.second << " * x^" << ind_first.first << ' ';
+    for (++counter; counter != p.ind.end(); ++counter)
         Polynom::print(ostream, *counter);
-    }
     return ostream;
 }
 
@@ -135,7 +125,7 @@ int main(){
     std::cout << x + y << "\n";
     std::cout << x - y << "\n";
     std::cout << x * y << "\n";
-    std::cout << x / 3.4 << "\n";
+    std::cout << x / 5 << "\n";
     std::cout << x << "\n";
     std::cout << y << "\n";
     return 0;
